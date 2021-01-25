1fc33f2179889006ae212f785c6af12502e512be
can <canbolukbas98@gmail.com>
2021-01-25 15:47:10 +0300

    minor changes in preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -1,12 +1,40 @@
+import glob
+import json
+
+def json_reader(path: str):
+    f = open(path, "r")
+    text = json.load(f)
+    f.close()
+    return text
+
+def json_saver(filename: str, file: str):
+    # Save df in JSON format.
+    f = open(filename, "w")
+    json.dump(file, f, indent=2)
+    f.close()
+
+def get_tokens(filepath: str):
+    return []
 
 def create_bag_of_words_model(email_paths, email_type):
-    pass
+    bag_of_words = dict()
+    for filepath in email_paths:
+        tokens = get_tokens(filepath)
+        for token in tokens:
+            if token in bag_of_words:
+                bag_of_words[token] += 1
+            else:
+                bag_of_words[token] = 1
+    json_saver("{}_emails_bag_of_words_model.json".format(email_type), bag_of_words)
+
 
 def get_data_paths(email_type: str):
     if email_type == "spam":
-        return []
+        spam_files = glob.glob("./dataset/training/spam/*.txt")
+        return spam_files
     else:
-        return []
+        legitimate_files = glob.glob("./dataset/training/legitimate/*.txt")
+        return legitimate_files
 
 def preprocess():
     print("preprocess is started")
